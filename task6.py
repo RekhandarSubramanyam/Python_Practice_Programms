50f62639cf686ac88bf5f484bcc1d9ea8259e97f
Subramanyam Rekhandar <53898299+RekhandarSubramanyam@users.noreply.github.com>
2022-07-18 19:39:02 +0530

    Create task6.py

---
+++ task6.py
@@ -0,0 +1,7 @@
+#Check whether a character is vowel or consonant
+ch = input("enter the characters:")
+
+if(ch == 'a' or ch == 'e' or ch == 'i' or ch== 'o' or ch=='u'):
+    print(ch ,"charater is vowel")
+else:
+    print(ch,"charater is consonant")
